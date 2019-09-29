5173225dcf1d2e323b633659afaed868badf8307
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-09-29 12:22:56 +0100

    Adds vue component that lists the navItems

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ const header = new Vue({
     mission: 'Lorem ipsum dolor amet pug ramps master cleanse echo park poke selfies air plant, helvetica cold-pressed tumblr VHS. Health goth meh iPhone marfa +1 fixie. Occupy before they sold out helvetica aesthetic humblebrag mlkshk single-origin coffee microdosing.',
   },
 });
+
+const navBar = new Vue({
+  el: '#navBar',
+  data: {
+    items: [
+      { text: 'T&Cs', link: '/t&cs' },
+      { text: 'Cart', link: '/cart' },
+    ],
+  },
+});
