2c016811e4ff9c5d59e085ff00350eceaa951ebf
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-09-29 11:43:02 +0100

    Changes heading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const header = new Vue({
   el: '#header',
   data: {
+    heading: 'New Arrivals',
     mission: 'Lorem ipsum dolor amet pug ramps master cleanse echo park poke selfies air plant, helvetica cold-pressed tumblr VHS. Health goth meh iPhone marfa +1 fixie. Occupy before they sold out helvetica aesthetic humblebrag mlkshk single-origin coffee microdosing tumeric activated charcoal. Hoodie banjo la croix VHS bespoke paleo asymmetrical microdosing blog disrupt hexagon bushwick waistcoat XOXO ethical. ',
   },
 });
