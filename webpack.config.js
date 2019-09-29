d3fdbd4f23de2486016719bf6dd66e730025bf20
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-09-29 14:02:11 +0100

    Moved FE code to FE folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './frontend/src/index.js',
   output: {
-    path: path.resolve('./build/'),
+    path: path.resolve('./frontend/build/'),
     filename: 'bundle.js',
   },
   module: {
