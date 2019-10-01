ef720ca772583b2d522018873bb8ea6ab544ab31
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-10-01 08:54:30 +0100

    Adds routes directory and uses the routes that have been defined

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,5 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world'));
+const itemRoutes = require('../routes/items.js');
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.use('/api/items', itemRoutes);
+
 app.listen(port, () => console.log('Hello console'));
